ca90daf5d936ebf0c33972e087016dd87a9ec9f9
shijiangshan <shijiangshan@bjknrt.com>
2021-12-16 13:51:32 +0800

    保存

--- AccelerateBall/Forms/FrmMenu.Designer.cs
+++ AccelerateBall/Forms/FrmMenu.Designer.cs
@@ -38,9 +38,9 @@ namespace AccelerateBall.Forms
             this.uiSwitch = new Sunny.UI.UISwitch();
             this.uiLabel1 = new Sunny.UI.UILabel();
             this.panelTop = new Infragistics.Win.Misc.UltraPanel();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.pictureBoxStartOpen = new System.Windows.Forms.PictureBox();
-            this.pictureBoxAllCheck = new System.Windows.Forms.PictureBox();
+            this.uiTableLayoutPanel1 = new Sunny.UI.UITableLayoutPanel();
+            this.uiImageButton1 = new Sunny.UI.UIImageButton();
+            this.uiImageButton2 = new Sunny.UI.UIImageButton();
             this.panelFill.ClientArea.SuspendLayout();
             this.panelFill.SuspendLayout();
             this.panelContent.ClientArea.SuspendLayout();
@@ -49,9 +49,9 @@ namespace AccelerateBall.Forms
             this.panelFillTop.SuspendLayout();
             this.panelTop.ClientArea.SuspendLayout();
             this.panelTop.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStartOpen)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAllCheck)).BeginInit();
+            this.uiTableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.uiImageButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiImageButton2)).BeginInit();
             this.SuspendLayout();
             // 
             // panelFill
@@ -68,7 +68,7 @@ namespace AccelerateBall.Forms
             this.panelFill.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelFill.Location = new System.Drawing.Point(0, 0);
             this.panelFill.Name = "panelFill";
-            this.panelFill.Size = new System.Drawing.Size(213, 318);
+            this.panelFill.Size = new System.Drawing.Size(172, 349);
             this.panelFill.TabIndex = 2;
             // 
             // panelContent
@@ -81,7 +81,7 @@ namespace AccelerateBall.Forms
             this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelContent.Location = new System.Drawing.Point(0, 78);
             this.panelContent.Name = "panelContent";
-            this.panelContent.Size = new System.Drawing.Size(211, 238);
+            this.panelContent.Size = new System.Drawing.Size(170, 269);
             this.panelContent.TabIndex = 3;
             // 
             // grid
@@ -98,7 +98,7 @@ namespace AccelerateBall.Forms
             this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.grid.Location = new System.Drawing.Point(0, 54);
             this.grid.Name = "grid";
-            this.grid.Size = new System.Drawing.Size(211, 184);
+            this.grid.Size = new System.Drawing.Size(170, 215);
             this.grid.TabIndex = 2;
             this.grid.Text = "ultraGrid1";
             // 
@@ -114,8 +114,9 @@ namespace AccelerateBall.Forms
             this.panelFillTop.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelFillTop.MinimumSize = new System.Drawing.Size(1, 1);
             this.panelFillTop.Name = "panelFillTop";
+            this.panelFillTop.Radius = 0;
             this.panelFillTop.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.panelFillTop.Size = new System.Drawing.Size(211, 54);
+            this.panelFillTop.Size = new System.Drawing.Size(170, 54);
             this.panelFillTop.Style = Sunny.UI.UIStyle.Custom;
             this.panelFillTop.TabIndex = 3;
             this.panelFillTop.Text = null;
@@ -131,10 +132,10 @@ namespace AccelerateBall.Forms
             this.uiSwitch.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.uiSwitch.InActiveText = "";
             this.uiSwitch.IsScaled = false;
-            this.uiSwitch.Location = new System.Drawing.Point(123, 14);
+            this.uiSwitch.Location = new System.Drawing.Point(101, 14);
             this.uiSwitch.MinimumSize = new System.Drawing.Size(1, 1);
             this.uiSwitch.Name = "uiSwitch";
-            this.uiSwitch.Size = new System.Drawing.Size(61, 27);
+            this.uiSwitch.Size = new System.Drawing.Size(58, 27);
             this.uiSwitch.Style = Sunny.UI.UIStyle.Custom;
             this.uiSwitch.TabIndex = 1;
             this.uiSwitch.ValueChanged += new Sunny.UI.UISwitch.OnValueChanged(this.uiSwitch_ValueChanged);
@@ -144,7 +145,7 @@ namespace AccelerateBall.Forms
             this.uiLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.uiLabel1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.uiLabel1.Location = new System.Drawing.Point(13, 9);
+            this.uiLabel1.Location = new System.Drawing.Point(4, 9);
             this.uiLabel1.Name = "uiLabel1";
             this.uiLabel1.Size = new System.Drawing.Size(96, 35);
             this.uiLabel1.Style = Sunny.UI.UIStyle.Custom;
@@ -157,60 +158,64 @@ namespace AccelerateBall.Forms
             // 
             // panelTop.ClientArea
             // 
-            this.panelTop.ClientArea.Controls.Add(this.tableLayoutPanel1);
+            this.panelTop.ClientArea.Controls.Add(this.uiTableLayoutPanel1);
             this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTop.Location = new System.Drawing.Point(0, 0);
             this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(211, 78);
+            this.panelTop.Size = new System.Drawing.Size(170, 78);
             this.panelTop.TabIndex = 2;
             // 
-            // tableLayoutPanel1
+            // uiTableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.pictureBoxStartOpen, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBoxAllCheck, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(211, 78);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.uiTableLayoutPanel1.ColumnCount = 2;
+            this.uiTableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.uiTableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.uiTableLayoutPanel1.Controls.Add(this.uiImageButton2, 1, 0);
+            this.uiTableLayoutPanel1.Controls.Add(this.uiImageButton1, 0, 0);
+            this.uiTableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uiTableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.uiTableLayoutPanel1.Name = "uiTableLayoutPanel1";
+            this.uiTableLayoutPanel1.RowCount = 1;
+            this.uiTableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.uiTableLayoutPanel1.Size = new System.Drawing.Size(170, 78);
+            this.uiTableLayoutPanel1.TabIndex = 0;
+            this.uiTableLayoutPanel1.TagString = null;
             // 
-            // pictureBoxStartOpen
+            // uiImageButton1
             // 
-            this.pictureBoxStartOpen.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxStartOpen.Image = global::AccelerateBall.Properties.Resources.开机加速;
-            this.pictureBoxStartOpen.Location = new System.Drawing.Point(105, 0);
-            this.pictureBoxStartOpen.Margin = new System.Windows.Forms.Padding(0);
-            this.pictureBoxStartOpen.Name = "pictureBoxStartOpen";
-            this.pictureBoxStartOpen.Size = new System.Drawing.Size(106, 78);
-            this.pictureBoxStartOpen.TabIndex = 1;
-            this.pictureBoxStartOpen.TabStop = false;
-            this.pictureBoxStartOpen.MouseEnter += new System.EventHandler(this.PictureBox_MouseEnter);
-            this.pictureBoxStartOpen.MouseLeave += new System.EventHandler(this.PictureBox_MouseLeave);
+            this.uiImageButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiImageButton1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uiImageButton1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiImageButton1.Image = global::AccelerateBall.Properties.Resources.开机加速;
+            this.uiImageButton1.ImageHover = global::AccelerateBall.Properties.Resources.开机加速选中;
+            this.uiImageButton1.ImagePress = global::AccelerateBall.Properties.Resources.开机加速选中;
+            this.uiImageButton1.ImageSelected = global::AccelerateBall.Properties.Resources.开机加速选中;
+            this.uiImageButton1.Location = new System.Drawing.Point(3, 3);
+            this.uiImageButton1.Name = "uiImageButton1";
+            this.uiImageButton1.Size = new System.Drawing.Size(79, 72);
+            this.uiImageButton1.TabIndex = 0;
+            this.uiImageButton1.TabStop = false;
             // 
-            // pictureBoxAllCheck
+            // uiImageButton2
             // 
-            this.pictureBoxAllCheck.BackColor = System.Drawing.Color.White;
-            this.pictureBoxAllCheck.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxAllCheck.Image = global::AccelerateBall.Properties.Resources.全面体检;
-            this.pictureBoxAllCheck.Location = new System.Drawing.Point(0, 0);
-            this.pictureBoxAllCheck.Margin = new System.Windows.Forms.Padding(0);
-            this.pictureBoxAllCheck.Name = "pictureBoxAllCheck";
-            this.pictureBoxAllCheck.Size = new System.Drawing.Size(105, 78);
-            this.pictureBoxAllCheck.TabIndex = 0;
-            this.pictureBoxAllCheck.TabStop = false;
-            this.pictureBoxAllCheck.MouseEnter += new System.EventHandler(this.PictureBox_MouseEnter);
-            this.pictureBoxAllCheck.MouseLeave += new System.EventHandler(this.PictureBox_MouseLeave);
+            this.uiImageButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiImageButton2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uiImageButton2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiImageButton2.Image = global::AccelerateBall.Properties.Resources.全面体检;
+            this.uiImageButton2.ImageHover = global::AccelerateBall.Properties.Resources.全面体检选中;
+            this.uiImageButton2.ImagePress = global::AccelerateBall.Properties.Resources.全面体检选中;
+            this.uiImageButton2.ImageSelected = global::AccelerateBall.Properties.Resources.全面体检选中;
+            this.uiImageButton2.Location = new System.Drawing.Point(88, 3);
+            this.uiImageButton2.Name = "uiImageButton2";
+            this.uiImageButton2.Size = new System.Drawing.Size(79, 72);
+            this.uiImageButton2.TabIndex = 1;
+            this.uiImageButton2.TabStop = false;
             // 
             // FrmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(213, 318);
+            this.ClientSize = new System.Drawing.Size(172, 349);
             this.Controls.Add(this.panelFill);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmMenu";
@@ -226,9 +231,9 @@ namespace AccelerateBall.Forms
             this.panelFillTop.ResumeLayout(false);
             this.panelTop.ClientArea.ResumeLayout(false);
             this.panelTop.ResumeLayout(false);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStartOpen)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAllCheck)).EndInit();
+            this.uiTableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.uiImageButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiImageButton2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -236,13 +241,13 @@ namespace AccelerateBall.Forms
         #endregion
         private Infragistics.Win.Misc.UltraPanel panelFill;
         private Infragistics.Win.Misc.UltraPanel panelTop;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.PictureBox pictureBoxStartOpen;
-        private System.Windows.Forms.PictureBox pictureBoxAllCheck;
         private Infragistics.Win.Misc.UltraPanel panelContent;
         private Infragistics.Win.UltraWinGrid.UltraGrid grid;
         private Sunny.UI.UIPanel panelFillTop;
         private Sunny.UI.UILabel uiLabel1;
         private Sunny.UI.UISwitch uiSwitch;
+        private Sunny.UI.UITableLayoutPanel uiTableLayoutPanel1;
+        private Sunny.UI.UIImageButton uiImageButton2;
+        private Sunny.UI.UIImageButton uiImageButton1;
     }
 }
--- AccelerateBall/Forms/FrmMenu.cs
+++ AccelerateBall/Forms/FrmMenu.cs
@@ -22,38 +22,6 @@ namespace AccelerateBall.Forms
 
         private void FrmMenu_MouseLeave(object sender, EventArgs e) => Hide();
 
-        private void PictureBox_MouseEnter(object sender, EventArgs e)
-        {
-            Cursor = Cursors.Hand;
-            if (sender is PictureBox pic)
-            {
-                if (pic.Name == pictureBoxAllCheck.Name)
-                {
-                    pic.Image = Properties.Resources.全面体检选中;
-                }
-                else if (pic.Name == pictureBoxStartOpen.Name)
-                {
-                    pic.Image = Properties.Resources.开机加速选中;
-                }
-            }
-        }
-
-        private void PictureBox_MouseLeave(object sender, EventArgs e)
-        {
-            Cursor = Cursors.Default;
-            if (sender is PictureBox pic)
-            {
-                if (pic.Name == pictureBoxAllCheck.Name)
-                {
-                    pic.Image = Properties.Resources.全面体检;
-                }
-                else if (pic.Name == pictureBoxStartOpen.Name)
-                {
-                    pic.Image = Properties.Resources.开机加速;
-                }
-            }
-        }
-
         private void LoadGrid()
         {
             grid.UpdateUI(() =>
